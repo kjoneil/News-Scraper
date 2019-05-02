d20b593af54aa1b33cbaef4791a35a36b105bff5
kjoneil <kjoneil.rw@gmail.com>
2019-05-01 21:02:35 -0500

    update

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 var express = require("express");
-var logger = require("morgan");
-var mongoose = require("mongoose");
-
-// Our scraping tools
-// Axios is a promised-based http library, similar to jQuery's Ajax method
-// It works on the client and on the server
+// var logger = require("morgan");
+var path = require('path');
+// var mongoose = require("mongoose");
 var axios = require("axios");
 var cheerio = require("cheerio");
 
 // Require all models
-var db = require("./models");
+// var db = require("./models");
 
 var PORT = 3000;
 
